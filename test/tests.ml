fc9b3b8279d21a8fa84b5c677202d3afbd1d7918
Rafi Levy <rafi.levy@sky.com>
2020-09-21 02:33:51 +0100

    Added + tests

--- test/tests.ml
+++ test/tests.ml
@@ -10,12 +10,20 @@ let compilation_test_Union_2 () = (compile "a|(b|c)") = Union(Char('a'), Union(C
 let compilation_test_Union_3 () = (compile "a|b|cd") = Union(Char('a'), Union(Char('b'), Concat(Char('c'), Char('d'))))
 let compilation_test_Union_4 () = (compile "|.") = Union(Empty, Wildcard)
 let compilation_test_Union_5 () = (compile ".|") = Union(Wildcard, Empty)
-let compilation_test_Star_0 () = (compile ".*") = Repeat(Wildcard)
-let compilation_test_Star_1 () = (compile "(.*)") = Repeat(Wildcard)
-let compilation_test_Star_2 () = (compile "a.*") = Concat(Char('a'), Repeat(Wildcard))
-let compilation_test_Star_3 () = (compile "(.a)*") = Repeat(Concat(Wildcard, Char('a')))
-let compilation_test_Star_4 () = (compile ".|(a*)") = Union(Wildcard, Repeat(Char('a')))
-let compilation_test_Star_5 () = (compile "(a|.)*") = Repeat(Union(Char('a'), Wildcard))
+let compilation_test_Star_0 () = (compile ".*") = Repeat(Wildcard, 0)
+let compilation_test_Star_1 () = (compile "(.*)") = Repeat(Wildcard, 0)
+let compilation_test_Star_2 () = (compile "a.*") = Concat(Char('a'), Repeat(Wildcard, 0))
+let compilation_test_Star_3 () = (compile "(.a)*") = Repeat(Concat(Wildcard, Char('a')), 0)
+let compilation_test_Star_4 () = (compile ".|(a*)") = Union(Wildcard, Repeat(Char('a'), 0))
+let compilation_test_Star_5 () = (compile "(a|.)*") = Repeat(Union(Char('a'), Wildcard), 0)
+
+
+let compilation_test_Plus_0 () = (compile ".+") = Repeat(Wildcard, 1)
+let compilation_test_Plus_1 () = (compile "(.+)") = Repeat(Wildcard, 1)
+let compilation_test_Plus_2 () = (compile "a.+") = Concat(Char('a'), Repeat(Wildcard, 1))
+let compilation_test_Plus_3 () = (compile "(.a)+") = Repeat(Concat(Wildcard, Char('a')), 1)
+let compilation_test_Plus_4 () = (compile ".|(a+)") = Union(Wildcard, Repeat(Char('a'), 1))
+let compilation_test_Plus_5 () = (compile "(a|.)+") = Repeat(Union(Char('a'), Wildcard), 1)
 
 let match_test_0 () = regex_match "" ""
 let match_test_1 a = regex_match "." a
@@ -29,6 +37,15 @@ let match_test_8 () = regex_match "a.a" "afa"
 let match_test_9 a = regex_match "a.a" ("a"^a^"a")
 let match_test_10 () = regex_match "a*" ""
 let match_test_11 () = regex_match "a*" "a"
-let match_test_12 () = regex_match "a*" "aa"
-let match_test_13 () = regex_match "a*" "aaaaaaaaaaaaaaaa"
+let match_test_12 () = regex_match "a*" "aaa"
+let match_test_13 () = regex_match "ca*b" "cb"
 let match_test_14 () = regex_match "(a|b)*" "abba"
+let match_test_15 () = not (regex_match "a+" "")
+let match_test_16 () = regex_match "a+" "a"
+let match_test_17 () = regex_match "a+b" "aab"
+let match_test_18 () = regex_match "a*b" "b"
+let match_test_19 () = regex_match "a+b" "aab"
+let match_test_20 () = regex_match "(a|b|c)+.*de" "abbbccfffde"
+let match_test_21 () = not (regex_match "(a|b|c)+.*de" "fffde")
+let match_test_22 () = (regex_match "(a|b|c)+.*de" "ababbccde")
+let match_test_23 () = not (regex_match "(a|b|c)+.*de" "abbbccfffd")
